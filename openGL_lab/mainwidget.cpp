9c46e378674e71f2378745e17bdb56ed72b21627
Nikolay <2012tafi2012@gmail.com>
2020-02-25 01:52:25 +0300

    Problems with collision

--- openGL_lab/mainwidget.cpp
+++ openGL_lab/mainwidget.cpp
@@ -6,28 +6,241 @@
 //#include <GLES3/gl3.h>
 #include <GL/gl.h>
 
-#define GL_PI 3.141592654f
+static const GLfloat GL_PI = 3.141592654f;
 
 using namespace LabaGL;
+
+//--------------------------------------SPHERE
+
+Sphere::Sphere(const QVector3D& position
+               , const GLfloat radius
+               , const QVector3D& interval)
+    : m_currentPosition{ position }
+    , m_radius{ radius }
+    , m_interval{ interval }
+{
+    m_colors.push_back(QColor(Qt::green));
+    m_colors.push_back(QColor(Qt::red));
+
+    vertexCalculation();
+}
+
+Sphere::~Sphere()
+{
+}
+
+void Sphere::draw()
+{
+    m_currentPosition += m_interval;
+
+    glTranslatef(m_currentPosition.x()
+                 ,  m_currentPosition.y()
+                 , m_currentPosition.z());
+
+    GLuint n = glGenLists(1);
+    glNewList(n, GL_COMPILE_AND_EXECUTE);
+
+    glBegin(/*GL_LINE_STRIP);//*/GL_TRIANGLE_STRIP);
+
+    bool checkColor = false;
+
+    for(auto vertex : m_vertex)
+    {
+        if(checkColor)
+        {
+            if(m_colors.empty())
+            {
+                glColor3f(1.0, 1.0, 1.0);
+            }
+            else
+            {
+                glColor3f(static_cast<GLfloat>(m_colors.at(0).redF()),
+                          static_cast<GLfloat>(m_colors.at(0).greenF()),
+                          static_cast<GLfloat>(m_colors.at(0).blueF()));
+            }
+            checkColor = false;
+        }
+        else
+        {
+            if(m_colors.size() > 1)
+            {
+                glColor3f(static_cast<GLfloat>(m_colors.at(1).redF()),
+                          static_cast<GLfloat>(m_colors.at(1).greenF()),
+                          static_cast<GLfloat>(m_colors.at(1).blueF()));
+            }
+            checkColor = true;
+        }
+        glVertex3f(vertex.x(), vertex.y(), vertex.z());
+    }
+
+    glEnd();
+
+    glEndList();
+
+    glCallList(n);
+
+    glLoadIdentity();
+}
+
+QVector3D Sphere::drivingVector() const
+{
+    return  QVector3D{};
+}
+
+void Sphere::setDrivingVector(const QVector3D &)
+{
+
+}
+
+float Sphere::speed() const
+{
+    return 0.0;
+}
+
+void Sphere::setSpeed(const float)
+{
+
+}
+
+QVector3D Sphere::currentPosition() const
+{
+    return QVector3D{};
+}
+
+std::vector<QVector3D> Sphere::pointsCurrentPosition() const
+{
+    std::vector<QVector3D> points;
+
+    for(auto point : m_vertex)
+    {
+        points.push_back(point + m_currentPosition);
+    }
+
+    return points;
+}
+
+std::vector<QColor> Sphere::colors() const
+{
+    return m_colors;
+}
+
+void Sphere::setColors( const std::vector<QColor>& colors)
+{
+    if(colors == m_colors)
+    {
+        return;
+    }
+
+    m_colors = colors;
+}
+
+void Sphere::vertexCalculation()
+{
+    GLfloat x, y, z, alpha, beta; // Storage for coordinates and angles
+
+    int gradation = 20;
+
+    for (alpha = 0.0; alpha < GL_PI; alpha += GL_PI/gradation)
+    {
+        for (beta = 0.0; beta < 2.01f*GL_PI; beta += GL_PI/gradation)
+        {
+            x = m_radius*cosf(beta)*sinf(alpha);
+            y = m_radius*sinf(beta)*sinf(alpha);
+            z = m_radius*cosf(alpha);
+
+            m_vertex.push_back( {x, y, z} );
+
+            x = m_radius*cosf(beta)*sinf(alpha + GL_PI/gradation);
+            y = m_radius*sinf(beta)*sinf(alpha + GL_PI/gradation);
+            z = m_radius*cosf(alpha + GL_PI/gradation);
+
+            m_vertex.push_back( {x, y, z} );
+        }
+        glEnd();
+    }
+}
+
+///--------------------------------------PLANE
+
+Plane::Plane()
+{
+
+}
+
+Plane::~Plane()
+{
+
+}
+
+void Plane::draw()
+{
+    GLuint n = glGenLists(1);
+
+    glNewList(n, GL_COMPILE_AND_EXECUTE);
+
+    GLfloat fSize = 0.7f;
+
+    glBegin(GL_QUADS);
+
+        glColor4f(1.0f, 0.0f, 0.0f, 1.0);
+        glVertex3f(-fSize, -fSize, fSize);
+        glVertex3f(fSize, -fSize, fSize);
+        glVertex3f(fSize, -fSize, -fSize);
+        glVertex3f(-fSize, -fSize, -fSize);
+
+    glEnd();
+
+    glEndList();
+
+    glCallList(n);
+
+    glLoadIdentity();
+}
+
+QVector3D Plane::drivingVector() const
+{
+    return QVector3D{};
+}
+
+void Plane::setDrivingVector(const QVector3D &)
+{
+
+}
+
+float Plane::speed() const
+{
+    return 0.0;
+}
+
+void Plane::setSpeed(const float)
+{
+
+}
+
+QVector3D Plane::currentPosition() const
+{
+    return QVector3D{};
+}
+
+std::vector<QVector3D> Plane::pointsCurrentPosition() const
+{
+    return std::vector<QVector3D>{};
+}
+
+//---------------------------------------WIDGET
 
 MainWidget::MainWidget(QWidget* parent)
     : QOpenGLWidget(parent)
     , m_xRotate{ 0 }
     , m_yRotate{ 0 }
-    , m_position0{1.0f, 0.3f, 0.2f}
-    , m_position1{0.2f, 1.0f, 0.2f}
-    , m_position2{0.2f, 0.3f, 0.8f}
+    , m_sphereLeft{ nullptr }
+    , m_sphereRight{ nullptr }
+    , m_bottom{ nullptr }
 {
+    initFigures();
 
     m_timer.setInterval(5);
     connect(&m_timer, &QTimer::timeout, this, [this](){
-        QVector3D interval{0.001f, 0.001f, 0.001f};
-
-        m_position0 -= interval;
-        m_position1 += interval;
-        interval.setY(0.0f);
-        m_position2 += interval;
-
         update();
     });
 
@@ -60,14 +273,32 @@ void MainWidget::paintGL()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glTranslatef(0.0, 0.0, -3.0);
-    glRotatef(m_xRotate, 1.0, 0.0, 0.0);
-    glRotatef(m_yRotate, 0.0, 1.0, 0.0);
 
-    glCallList(drawSphere(m_position0));
-    glCallList(drawSphere(m_position1));
-    glCallList(drawSphere(m_position2));
+    glLoadIdentity();   ///задаёт матрице проектирования единичную матрицу
+
+    {
+        glTranslatef(0.0, 0.0, -3.0);
+        glRotatef(m_xRotate, 1.0, 0.0, 0.0);
+        glRotatef(m_yRotate, 0.0, 1.0, 0.0);
+
+        m_sphereLeft->draw();
+    }
+
+    {
+        glTranslatef(0.0, 0.0, -3.0);
+        glRotatef(m_xRotate, 1.0, 0.0, 0.0);
+        glRotatef(m_yRotate, 0.0, 1.0, 0.0);
+
+        m_sphereRight->draw();
+    }
+
+    {
+        glTranslatef(0.0, 0.0, -3.0);
+        glRotatef(m_xRotate, 1.0, 0.0, 0.0);
+        glRotatef(m_yRotate, 0.0, 1.0, 0.0);
+
+        m_bottom->draw();
+    }
 }
 
 void MainWidget::mousePressEvent(QMouseEvent *e)
@@ -87,58 +318,54 @@ void MainWidget::mouseMoveEvent(QMouseEvent *e)
     }
 }
 
-GLuint MainWidget::drawSphere(QVector3D point, GLfloat radius)
+void MainWidget::update()
 {
-    GLuint n = glGenLists(1);
+    const bool isCollision = collision(m_sphereLeft.get(), m_sphereRight.get());
 
-    glNewList(n, GL_COMPILE_AND_EXECUTE);
-
-    const float PI = 3.141592f;
-    GLfloat x, y, z, alpha, beta; // Storage for coordinates and angles
-
-    int gradation = 20;
-
-    float x0 = point.x();
-    float y0 = point.y();
-    float z0 = point.z();
-
-    GLfloat fSize = 0.7f;
-
-    for (alpha = 0.0; alpha < GL_PI; alpha += GL_PI/gradation)
+    if(isCollision)
     {
-        glColor3f(0.0, 1.0, 0.0);
+        std::vector<QColor> colors;
 
-        glBegin(/*GL_LINE_STRIP);//*/GL_TRIANGLE_STRIP);
-        for (beta = 0.0; beta < 2.01f*GL_PI; beta += GL_PI/gradation)
-        {
-            x = radius*cosf(beta)*sinf(alpha);
-            y = radius*sinf(beta)*sinf(alpha);
-            z = radius*cosf(alpha);
+        colors.push_back(QColor(Qt::gray));
+        colors.push_back(QColor(Qt::yellow));
 
-            glColor3f(0.0, 1.0, 0.0);
-            glVertex3f(x + x0, y + y0, z + z0);
-
-            x = radius*cosf(beta)*sinf(alpha + GL_PI/gradation);
-            y = radius*sinf(beta)*sinf(alpha + GL_PI/gradation);
-            z = radius*cosf(alpha + GL_PI/gradation);
-
-            glColor3f(0.0, 0.0, 1.0);
-            glVertex3f(x + x0, y + y0, z + z0);
-        }
-        glEnd();
+        m_sphereLeft->setColors(colors);
     }
 
-        glBegin(GL_QUADS);
+    QOpenGLWidget::update();
+}
 
-            glColor4f(1.0f, 0.0f, 0.0f, 1.0);
-            glVertex3f(-fSize, -fSize, fSize);
-            glVertex3f(fSize, -fSize, fSize);
-            glVertex3f(fSize, -fSize, -fSize);
-            glVertex3f(-fSize, -fSize, -fSize);
+void MainWidget::initFigures()
+{
+    m_sphereLeft = std::make_unique<Sphere>( QVector3D{ -1, 0, 0 }, 0.7f, QVector3D{0.01f, 0, 0} );
+    m_sphereRight = std::make_unique<Sphere>( QVector3D{ 1, 0, 0 }, 0.4f, QVector3D{-0.001f, 0, 0} );
+    m_bottom = std::make_unique<Plane>();
+}
 
-        glEnd();
+bool MainWidget::collision(const Figure* figure1, const Figure* figure2)
+{
+    //sqrt((x1-x2)^2+(y1-y2)^2+(z1-z2)^2)-R1-R2
+    const auto points1 = figure1->pointsCurrentPosition();
+    const auto points2 = figure2->pointsCurrentPosition();
 
-    glEndList();
+    const auto r1 = 0.7f;
+    const auto r2 = 0.4f;
 
-    return n;
+    for(const auto& point1 : points1)
+    {
+        for(const auto& point2 : points2)
+        {
+            auto d = sqrtf(
+                   (point1.x()-point2.x()) * (point1.x()-point2.x())
+                 + (point1.y()-point2.y()) * (point1.y()-point2.y())
+                 + (point1.z()-point2.z()) * (point1.z()-point2.z()))
+                 - r1-r2;
+            if( d <= 0)
+            {
+                //return true;
+            }
+        }
+    }
+
+    return  false;
 }
--- openGL_lab/mainwidget.h
+++ openGL_lab/mainwidget.h
@@ -6,7 +6,92 @@
 #include <QVector3D>
 #include <QTimer>
 
-namespace LabaGL {
+#include <memory>
+
+namespace LabaGL
+{
+
+class Figure
+{
+public:
+    virtual ~Figure() = default;
+
+    virtual void draw() = 0;
+
+    virtual QVector3D drivingVector() const = 0;
+    virtual void setDrivingVector(const QVector3D&) = 0;
+
+    virtual float speed() const = 0;
+    virtual void setSpeed(const float) = 0;
+
+    virtual QVector3D currentPosition() const = 0;
+    virtual std::vector<QVector3D> pointsCurrentPosition() const = 0;
+
+    virtual std::vector<QColor> colors() const { return std::vector<QColor>{}; }
+    virtual void setColors( const std::vector<QColor>& ){}
+};
+
+class Sphere
+        : public Figure
+{
+public:
+    Sphere(const QVector3D& position
+           , const GLfloat radius
+           , const QVector3D& interval = {0.0, 0.0, 0.0});
+
+    ~Sphere() override;
+
+public:
+    virtual void draw() override;
+
+    virtual QVector3D drivingVector() const override;
+    virtual void setDrivingVector(const QVector3D&) override;
+
+    virtual float speed() const override;
+    virtual void setSpeed(const float) override;
+
+    virtual QVector3D currentPosition() const override;
+    virtual std::vector<QVector3D> pointsCurrentPosition() const override;
+
+    virtual std::vector<QColor> colors() const override;
+    virtual void setColors( const std::vector<QColor>& ) override;
+
+private:
+    void vertexCalculation();
+
+private:
+    QVector3D m_currentPosition;
+    GLfloat m_radius;
+    QVector3D m_interval;
+    std::vector<QVector3D> m_vertex;
+
+    std::vector<QColor> m_colors;
+
+};
+
+class Plane
+        : public Figure
+{
+public:
+    Plane();
+    ~Plane() override;
+
+public:
+    virtual void draw() override;
+
+    virtual QVector3D drivingVector() const override;
+    virtual void setDrivingVector(const QVector3D&) override;
+
+    virtual float speed() const override;
+    virtual void setSpeed(const float) override;
+
+    virtual QVector3D currentPosition() const override;
+    virtual std::vector<QVector3D> pointsCurrentPosition() const override;
+
+private:
+    QVector3D m_position;
+
+};
 
 class MainWidget
         : public QOpenGLWidget
@@ -24,7 +109,11 @@ protected:
     virtual void mousePressEvent(QMouseEvent* e) override;
     virtual void mouseMoveEvent(QMouseEvent* e) override;
 
-    GLuint drawSphere(QVector3D point = QVector3D{0, 0, 0}, GLfloat radius = 0.5);
+    virtual void update();
+
+private:
+    void initFigures();
+    bool collision(const Figure* figure1, const Figure* figure2);
 
 private:
     GLuint m_nPyramid;
@@ -32,11 +121,11 @@ private:
     GLfloat m_yRotate;
     QPoint m_ptPositione;
 
-    QVector3D m_position0;
-    QVector3D m_position1;
-    QVector3D m_position2;
-
     QTimer m_timer;
+
+    std::unique_ptr<Figure> m_sphereLeft;
+    std::unique_ptr<Figure> m_sphereRight;
+    std::unique_ptr<Figure> m_bottom;
 
 };
 
